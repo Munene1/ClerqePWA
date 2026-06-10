42ee74200c6fca77b5ca0edbcd3f36226804382f
Munene1 <pheneasm438@gmail.com>
2026-06-10 21:11:33 +0300

    hide browser logs in prod, disable sourcemaps, mask URLs in errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,32 @@
+function sanitizeUrl(msg: string): string {
+  return msg.replace(/https?:\/\/[^\s"'>)\]},;:]+/g, "[URL]");
+}
+
+function sanitizeArgs(args: unknown[]): unknown[] {
+  return args.map((a) => {
+    if (typeof a === "string") return sanitizeUrl(a);
+    if (a instanceof Error) {
+      const err = a;
+      try {
+        return new Error(sanitizeUrl(err.message));
+      } catch {
+        return a;
+      }
+    }
+    return a;
+  });
+}
+
+if (import.meta.env.PROD) {
+  const noop = () => {};
+  console.debug = noop;
+  console.log = noop;
+  console.info = noop;
+  console.warn = noop;
+  const origError = console.error;
+  console.error = (...args) => origError(...sanitizeArgs(args));
+}
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
